abd839658db4f6674aadfa4f216b40169c99deb8
Ben-Abdul <thenixadm@gmail.com>
2020-04-07 02:03:52 +0000

    Introduced Spouse instance & Siblings list and populate the Main class
    Each node will have a spouse instance that will contain its spouse and also an ArrayList of node being their siblings. The main issue is that once a node is created, it cannot refer to a sibling our spouse that hasn't been created yet.
    
    Signed-off-by: Ben-Abdul <thenixadm@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("Jeanne", "Jane", "Gagnoa", 102, new Date(), null);
+        Person p2 = new Person("Martin", "Jane", "Gagnoa", 102, new Date(), null);
+        Person p3 = new Person("Myriam", "Jane", "Gagnoa", 102, new Date(), null);
+        Person p4 = new Person("Ping", "Pong", "Gagnoa", 102, new Date(), null);
+        Person p5 = new Person("Esther", "Jane", "Gagnoa", 80, new Date(), null);
+        Person p6 = new Person("Ben", "Jane", "Gagnoa", 78, new Date(), null);
+        Person p7 = new Person("Rita", "Jane", "Gagnoa", 84, new Date(), null);
+        Person p8 = new Person("John", "Jane", "Gagnoa", 42, new Date(), null);
 
+        Node<Person> n1 = new Node<>(p1);
+        Node<Person> n2 = new Node<>(p2);
+        Node<Person> n3 = new Node<>(p3);
+        Node<Person> n4 = new Node<>(p4);
+        Node<Person> n5 = new Node<>(p5, 1, n1, n2, null, null);
+        Node<Person> n6 = new Node<>(p6, 1, n3, n4, n5, null);
+        Node<Person> n7 = new Node<>(p7, 1, n4, n5, null, new ArrayList<Node<Person>>(){{add(n6);}});
+        Node<Person> n8 = new Node<>(p8, 2, n5, n6, null, null);
+
+        // Setting missing spouse
+        n5.setSpouse(n6);
+
+    }
 }
